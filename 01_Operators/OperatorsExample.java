e48d3d5eb265bf938634d986901634e6bf229977
sujalkumar27 <sujal31122005@gmail.com>
2025-09-05 23:59:33 +0530

    Operators

---
+++ 01_Operators/OperatorsExample.java
@@ -0,0 +1,12 @@
+public class OperatorsEx1 {
+
+    public static void main(String[] args) {
+        int a=7/2;
+        System.out.println(a);
+        double b=7/2;
+        System.out.println(b);
+        double c=7/2.0;
+        System.out.println(c);
+    }
+
+}
