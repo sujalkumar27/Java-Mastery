776ed54da1561ea65855e19efb013cace0614b17
sujalkumar27 <sujal31122005@gmail.com>
2025-09-14 22:05:56 +0530

    switch statement

---
+++ 04_DecisionMaking(If else)/SwitchQues2.java
@@ -0,0 +1,16 @@
+
+public class SwitchQues2 {
+	public static void main(String[] args) {
+		switch("mohan") {
+			case "ram":
+				System.out.println("Hi");
+				break;
+			case "mohan":
+				System.out.println("Hello");
+				break;
+			default:
+				System.out.println("Invalid No.!");
+		}
+	}
+
+}
